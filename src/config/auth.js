86b472dc17e1c5a0c1c55c9a12936b155ea37405
Everton Reis <evertonreis@Mac-mini-de-Everton.local>
2019-11-14 21:36:20 -0400

    Autenticação JWT

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'f4ba1a56f7cebe9f70a2cecd4a20c827',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@
 // usando o sucrase podemos utilisar:
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 // module.exports = routes
 
 // usando o sucrase podemos utilisar:
